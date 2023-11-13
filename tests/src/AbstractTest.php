112f4ff15ec9d878661d897c94a108d6a3bcb206
Gabriel Solomon <hello@gabrielsolomon.ro>
2023-11-14 00:26:21 +0200

    initial commit

---
+++ tests/src/AbstractTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Marktic\Billing\Tests;
+
+use Bytic\Phpqa\PHPUnit\TestCase;
+use Mockery;
+
+/**
+ * Class AbstractTest
+ */
+abstract class AbstractTest extends TestCase
+{
+
+}
